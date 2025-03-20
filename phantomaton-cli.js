5ab1605e2f759636a90712ddf9d8c67884c9f240
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-20 15:20:49 -0700

    trim

--- phantomaton-cli.js
+++ phantomaton-cli.js
@@ -1,13 +1,11 @@
 import conversations from 'phantomaton-conversations';
 import priestess from 'priestess';
 
-import assistant from './assistant.js';
 import start from './start.js';
 import user from './user.js';
 
 const cli = () => ({
   install: [    
-    conversations.assistant.decorator([], () => assistant),
     conversations.user.provider([], user),
     priestess.start.provider(
       [conversations.conversation.resolve],
